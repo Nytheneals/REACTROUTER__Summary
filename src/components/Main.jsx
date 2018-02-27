4a4a23fe08c27c1ae321e73a44aea916c23e2e41
Nytheneals <nythes@gmail.com>
2018-02-27 08:49:18 -0500

    Added Routing

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,25 +2,67 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 // COMPONENTS
-// import PhotoGrid from "./PhotoGrid";
-// import SingleView from "./SingleView";
-// CSS
-import '../css/_animations.css';
-import '../css/_normalise.css';
-import '../css/_style.css';
-import '../css/_topography.css';
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Home Alone!!</h1>
+      </div>
+    );
+  }
+}
 
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About What!!</h1>
+      </div>
+    );
+  }
+}
+
+class Topics extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Topics</h1>
+        <li>
+          <Link to="/topics/rendering"> Rendering with react</Link>
+        </li>
+        <li>
+          <Link to="/topics/components"> Component</Link>
+        </li>
+        <li>
+          <Link to="/topics/props-v-state"> Props V State</Link>
+        </li>
+        <h1>Please select the topic</h1>
+      </div>
+    );
+  }
+}
 // COMPONENT
 class Main extends Component {
   render() {
     return (
       <Router>
-        <Route>
-          <h1>
-            <Link to="/">Kirste-gram</Link>
-            {React.cloneElement(this.props.children, this.props)}
-          </h1>
-        </Route>
+        <div>
+          <ul>
+            <li className="_link">
+              <Link to="/"> Home</Link>
+            </li>
+            <li className="_link">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="_link">
+              <Link to="/topics"> Topics</Link>
+            </li>
+          </ul>
+          <hr />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topics} />
+        </div>
       </Router>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './css/_animations.css';
-import './css/_normalise.css';
-import './css/_style.css';
-import './css/_topography.css';
+
 import Main from './components/Main';
 // import registerServiceWorker from './registerServiceWorker';
 
